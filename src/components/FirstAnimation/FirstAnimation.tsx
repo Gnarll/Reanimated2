3b8ecb4f4aee8061d6f2a6ff74c02c64399ce3f0
Gnarll <vadimfedorovich22@mail.ru>
2022-04-27 15:38:43 +0300

    add first animation

--- src/components/FirstAnimation/FirstAnimation.tsx
+++ src/components/FirstAnimation/FirstAnimation.tsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withSpring } from "react-native-reanimated";
 
-const SIZE = 150
+const SIZE: number = 100
 
 
 export const FirstAnimation = () => {
-    const progress = useSharedValue(0)
+    const progress = useSharedValue(1) 
+    const scale = useSharedValue(2)
     const animatedStyles = useAnimatedStyle(() => {
-        return {
-            transform: {}
-        }
-    })
+        return { 
+            transform: [{scale: scale.value}, {rotate: `${progress.value * 2 * Math.PI}rad`} ],
+            opacity: progress.value,
+            borderRadius: progress.value * SIZE / 2
+        } 
+    },)
+
+    useEffect(() => {
+        progress.value = withRepeat(withSpring(0.5), -1, true)
+        scale.value = withRepeat(withSpring(1), -1, true)
+    }, [])
+
   return <View style={styles.container}>
-        <Animated.View style={[styles.figure]}>
-        </Animated.View>
+        <Animated.View style={[styles.figure, animatedStyles]} />
      </View>
 }
 
